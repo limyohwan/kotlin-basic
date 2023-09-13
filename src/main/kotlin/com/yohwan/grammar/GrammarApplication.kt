f273469a479fcdc18ad44ad0544ec66b8b7e4909
Yohwan Lim <dyghks7102@naver.com>
2023-09-13 22:44:56 +0900

    first commit

---
+++ src/main/kotlin/com/yohwan/grammar/GrammarApplication.kt
@@ -0,0 +1,11 @@
+package com.yohwan.grammar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrammarApplication
+
+fun main(args: Array<String>) {
+	runApplication<GrammarApplication>(*args)
+}
