4a11d2f557161e66ed510f99877f1c13d9cbbdb2
Yohwan Lim <dyghks7102@naver.com>
2023-09-13 23:14:24 +0900

    feat. 형변환

---
+++ src/main/kotlin/com/yohwan/grammar/basic/TypeConversion.kt
@@ -0,0 +1,12 @@
+package com.yohwan.grammar.basic
+
+fun main() {
+    var i = 10
+    var l = 20L
+    var name = "15"
+
+    l = i.toLong()
+    i = name.toInt()
+    name = i.toString()
+    // 변수명.toXXX()
+}
