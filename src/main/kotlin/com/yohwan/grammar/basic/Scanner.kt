09b2a5c35322f04c24c8ac210ac695f40006e3a3
Yohwan Lim <dyghks7102@naver.com>
2023-09-14 23:05:04 +0900

    feat. 스캐너 정리

---
+++ src/main/kotlin/com/yohwan/grammar/basic/Scanner.kt
@@ -0,0 +1,9 @@
+package com.yohwan.grammar.basic
+
+import java.util.*
+
+fun main() {
+    val reader = Scanner(System.`in`) // kotlin 에서 in은 사용할 수 없는 키워드임
+    val arg = reader.next()
+    println(arg)
+}
