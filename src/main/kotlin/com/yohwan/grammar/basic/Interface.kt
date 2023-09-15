4308a3526971ae53f7bb36c5a0ce79b0d45e1c97
Yohwan Lim <dyghks7102@naver.com>
2023-09-15 23:48:06 +0900

    feat. type check는 is 를 사용함

--- src/main/kotlin/com/yohwan/grammar/basic/Interface.kt
+++ src/main/kotlin/com/yohwan/grammar/basic/Interface.kt
@@ -1,7 +1,26 @@
 package com.yohwan.grammar.basic
 
 fun main() {
+    val tiger: Animal = Tiger()
+    val lion = Lion()
 
+    if(tiger is Tiger) {
+        println("I am tiger")
+        tiger.run()
+        tiger.move()
+    }
+
+    if(tiger is Animal) {
+        println("I am Animal")
+        tiger.move()
+        // tiger.run() Animal 타입이기 때문에 run()은 없음
+    }
+
+    if(tiger is Lion) {
+        println("I am lion")
+    } else {
+        println("I am not lion")
+    }
 }
 
 interface Runnable {
