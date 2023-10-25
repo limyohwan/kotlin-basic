08556ec8a6c76abb55512fd5a2fcd8b0e2c1545b
Yohwan Lim <dyghks7102@naver.com>
2023-10-25 23:35:24 +0900

    feat. if문과 when문 추가

--- src/main/kotlin/com/yohwan/grammar/kotlininaction/Expr.kt
+++ src/main/kotlin/com/yohwan/grammar/kotlininaction/Expr.kt
@@ -10,18 +10,35 @@ fun eval(e: Expr) : Int {
         return n.value
     }
 
-    if(e is Sum) {
+    if (e is Sum) {
         return eval(e.right) + eval(e.left) // 변수 e에 대해 스마트 캐스트를 사용, 인텔리제이가 배경색으로 스마트 캐스팅한 부분을 표시해줌
     }
 
     throw IllegalArgumentException("Unknown expression")
 }
-
 // 스마트 캐스트 (smart cast)
 // 코틀린에서는 프로그래머 대신 컴파일러가 캐스팅 해줌
 // 어떤 변수가 원하는 타입인지 일단 is로 검사하고 나면 굳이 변수를 원하는 타입으로 캐스팅하지 않아도 마치 처음 부터 그 변수가 원하는 타입으로 선언된 것처럼 사용할 수 있음
 // 실제로는 컴파일러가 캐스팅을 수행해줌
 // 스마트 캐스트를 사용하기 위해서는 값이 변할 수 없는 val일 경우에만 가능함
+
+fun eval2(e: Expr) : Int =
+    if (e is Num) {
+        e.value
+    } else if (e is Sum) {
+        eval(e.right) + eval(e.left) // 변수 e에 대해 스마트 캐스트를 사용, 인텔리제이가 배경색으로 스마트 캐스팅한 부분을 표시해줌
+    } else {
+        throw IllegalArgumentException("Unknown expression")
+    }
+// if문 분기로 수정
+
+fun eval3(e: Expr) : Int =
+    when (e) {
+        is Num -> e.value
+        is Sum -> eval(e.right) + eval(e.left) // 변수 e에 대해 스마트 캐스트를 사용, 인텔리제이가 배경색으로 스마트 캐스팅한 부분을 표시해줌
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
+// when 으로 수정
 
 fun main(args: Array<String>) {
     println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
