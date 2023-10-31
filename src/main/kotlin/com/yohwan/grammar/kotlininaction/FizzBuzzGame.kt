9da92108bd98c214e0b437691d07e542db11826b
Yohwan Lim <dyghks7102@naver.com>
2023-10-31 23:05:18 +0900

    feat. for문 Fizzbuzz 게임

---
+++ src/main/kotlin/com/yohwan/grammar/kotlininaction/FizzBuzzGame.kt
@@ -0,0 +1,26 @@
+package com.yohwan.grammar.kotlininaction
+
+fun fizzBuzz(i: Int) = when {
+    i % 15 == 0 -> "FizzBuzz"
+    i % 3 == 0 -> "Fizz"
+    i % 5 == 0 -> "Buzz"
+    else -> "$i"
+}
+
+fun main(args: Array<String>) {
+    playDifficultFizzBuzz()
+}
+
+//일반 피즈버즈 게임
+fun playFizzBuzz() {
+    for (i in 1..100) {
+        println(fizzBuzz(i))
+    }
+}
+
+// 100부터 거꾸로 짝수만 세는 피즈버즈 게임
+fun playDifficultFizzBuzz() {
+    for (i in 100 downTo 1 step 2) {
+        println(fizzBuzz(i))
+    }
+}
