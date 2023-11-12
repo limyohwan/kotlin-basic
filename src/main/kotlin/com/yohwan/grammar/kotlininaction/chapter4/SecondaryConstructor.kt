8afa1170d374f621c9857d8013639ac9aa9cdf1b
Yohwan Lim <dyghks7102@naver.com>
2023-11-12 22:17:41 +0900

    feat: 생성자와 부생성자

---
+++ src/main/kotlin/com/yohwan/grammar/kotlininaction/chapter4/SecondaryConstructor.kt
@@ -0,0 +1,28 @@
+package com.yohwan.grammar.kotlininaction.chapter4
+
+import javax.naming.Context
+import javax.swing.text.AttributeSet
+
+open class Viewer {
+    constructor(ctx: Context) {
+
+    }
+
+    constructor(ctx: Context, attr: AttributeSet) { // 부 생성자
+
+    }
+}
+
+class ViewerButton: Viewer {
+    constructor(ctx: Context): super(ctx) { // super 키워드를 통해 상위 클래스의 생성자 호출, 상위 클래스 생성자에게 객체 생성을 위임함
+
+    }
+
+    constructor(ctx: Context): this(ctx, attr) { // this 키워드를 사용해서 자신의 다른 생성자를 호출하게 할 수 있음
+
+    }
+
+    constructor(ctx: Context, attr: AttributeSet): super(ctx, attr) { // 상위 클래스의 생성자 호출
+
+    }
+}
