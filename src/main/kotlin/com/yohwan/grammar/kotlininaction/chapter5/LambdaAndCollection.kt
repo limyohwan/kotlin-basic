45f3ee1dd0c813dbbf842fccaaf4bd4f0ff559f1
Yohwan Lim <dyghks7102@naver.com>
2023-11-18 23:21:24 +0900

    feat: 컬렉션 라이브러리 함수를 이용하여 검색하기

--- src/main/kotlin/com/yohwan/grammar/kotlininaction/chapter5/LambdaAndCollection.kt
+++ src/main/kotlin/com/yohwan/grammar/kotlininaction/chapter5/LambdaAndCollection.kt
@@ -3,6 +3,9 @@ package com.yohwan.grammar.kotlininaction.chapter5
 fun main(args: Array<String>) {
     val people = listOf(Person("Alice", 23), Person("Bob", 28))
     findTheOldest(people)
+
+    println(people.maxBy { it.age })
+    println(people.maxBy(Person::age))
 }
 
 data class Person(val name: String, val age: Int)
