11d62df92a1a22c7adbaaca029d909d740435091
Yohwan Lim <dyghks7102@naver.com>
2023-11-19 00:07:03 +0900

    feat: 람다식은 변수로 선언할 수 있으며 다양한 형식으로 사용할 수 있음

--- src/main/kotlin/com/yohwan/grammar/kotlininaction/chapter5/LambdaAndCollection.kt
+++ src/main/kotlin/com/yohwan/grammar/kotlininaction/chapter5/LambdaAndCollection.kt
@@ -4,8 +4,19 @@ fun main(args: Array<String>) {
     val people = listOf(Person("Alice", 23), Person("Bob", 28))
     findTheOldest(people)
 
+    // 다 같은 결과값
     println(people.maxBy { it.age })
     println(people.maxBy(Person::age))
+    println(people.maxBy({ p: Person -> p.age }))
+    println(people.maxBy() { p: Person -> p.age })
+    println(people.maxBy { p: Person -> p.age })
+
+    val sum = { x: Int, y: Int -> x + y}
+    println(sum(1,2))
+    run { println(42) }
+
+    val printNumber = { println(52) }
+    printNumber()
 }
 
 data class Person(val name: String, val age: Int)
