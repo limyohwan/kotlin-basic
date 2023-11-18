f2bea448bcf7ba92e1830711ca2532fa0abfb1fd
Yohwan Lim <dyghks7102@naver.com>
2023-11-18 23:19:57 +0900

    feat: 메소드를 만들어 컬렉션을 직접 검색하기

---
+++ src/main/kotlin/com/yohwan/grammar/kotlininaction/chapter5/LambdaAndCollection.kt
@@ -0,0 +1,21 @@
+package com.yohwan.grammar.kotlininaction.chapter5
+
+fun main(args: Array<String>) {
+    val people = listOf(Person("Alice", 23), Person("Bob", 28))
+    findTheOldest(people)
+}
+
+data class Person(val name: String, val age: Int)
+
+fun findTheOldest(people: List<Person>) {
+    var maxAge = 0
+    var theOldest: Person? = null
+    for (person in people) {
+        if (person.age > maxAge) {
+            maxAge = person.age
+            theOldest = person
+        }
+    }
+
+    println(theOldest)
+}
