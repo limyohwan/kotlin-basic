051e1465ba4a53259462bf7a3153e6950e1866b8
Yohwan Lim <dyghks7102@naver.com>
2023-11-24 23:42:10 +0900

    feat: 타입 파라미터의 널가능성

--- src/main/kotlin/com/yohwan/grammar/kotlininaction/chapter6/Nullability2.kt
+++ src/main/kotlin/com/yohwan/grammar/kotlininaction/chapter6/Nullability2.kt
@@ -4,7 +4,11 @@ import org.springframework.util.Assert
 
 // 6.1.8 ~
 fun main(args: Array<String>) {
+    verifyUserInput("")
+    verifyUserInput(null)
 
+    printHashCode(null) // T의 타입은 Any?로 추론됨
+//    printNonNullHashCode(null) // null을 허용하지 않아 컴파일 에러 발생
 }
 
 class MyService {
@@ -37,4 +41,18 @@ class MyTest2 {
     fun testAction() {
         Assert.hasText("foo", myService.performAction()) // 널 검사를 하지 않아도 프로퍼티 사용 가능
     }
+}
+
+fun verifyUserInput(input: String?) {
+    if (input.isNullOrBlank()) { // null인 경우 true를 반환하고, null이 아닌 경우 isBlank를 실행함
+        println("Please fill in the required fields")
+    }
+}
+
+fun <T> printHashCode(t : T) { // 타입 파라미터 T는 기본적으로 null이 될 수 있는 타입임
+    println(t?.hashCode()) // t 가 null이 될 수 있는 타입이므로 안전한 호출을 써야함
+}
+
+fun <T: Any> printNonNullHashCode(t : T) {
+    println(t.hashCode()) // t 가 null이 될 수 없음
 }
