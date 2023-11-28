4c26ee689b16cb0b043c0f11b816d60d82a382c3
Yohwan Lim <dyghks7102@naver.com>
2023-11-28 22:15:52 +0900

    feat: 복합대입연산자, 단항연산자 정리

---
+++ src/main/kotlin/com/yohwan/grammar/kotlininaction/chapter7/CompoundAssignment.kt
@@ -0,0 +1,42 @@
+package com.yohwan.grammar.kotlininaction.chapter7
+
+import java.math.BigDecimal
+
+// +=, -= 등의 연산자를 복합 대입 연산자라 부름
+fun main(args: Array<String>) {
+    var point = Point(1, 2)
+    point += Point(3, 4) // 객체에 대한 참조를 다른 참조로 바꿈
+    println(point)
+
+    val numbers = ArrayList<Int>()
+    numbers += 42
+    println(numbers[0])
+
+    val list = arrayListOf(1, 2)
+    list += 3 // +=는 list를 변경함
+    val newList = list + listOf(4,5) // + 는 두 리스트의 모든 원소를 포함하는 새로운 리스트를 반환함
+    println(list)
+    println(newList)
+
+    val p = Point(10, 20)
+    println(-p)
+
+    var bd = BigDecimal.ZERO
+    println(bd++) // 후위 증가 연산자는 println이 실행된 다음에 값을 증가함
+    println(++bd) // 전위 증가 연산자는 println이 실행되기 전에 값을 증가함
+}
+
+operator fun <T> MutableCollection<T>.plusAssign(element: T) { // plusAssign을 정의하면 += 연산자에 이 함수를 사용함, minusAssign, timesAssign 등도 복합 대입 연산자로 사용됨
+    this.add(element)
+}
+
+operator fun Point.unaryMinus() : Point { // 단항 연산자
+    return Point(-x, -y)
+}
+
+// +a   unaryPlus
+// -a   unaryMinus
+// !a   not
+// ++a, a++  inc
+// --a, a-- dec
+operator fun BigDecimal.inc() = this + BigDecimal.ONE
