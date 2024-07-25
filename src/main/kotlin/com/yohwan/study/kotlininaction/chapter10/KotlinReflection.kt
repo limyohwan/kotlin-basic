acae803fc2c6191cbf2c83f8b19ac83f970e26ee
Yohwan Lim <dyghks7102@naver.com>
2024-07-25 23:23:21 +0900

    feat: 리플렉션 개념 정리

---
+++ src/main/kotlin/com/yohwan/study/kotlininaction/chapter10/KotlinReflection.kt
@@ -0,0 +1,12 @@
+package com.yohwan.study.kotlininaction.chapter10
+
+fun main(args: Array<String>) {
+
+}
+
+/*
+* Reflection = 실행 시점에 (동적으로) 객체의 프로퍼티와 메소드에 접근할 수 있게 해줌
+* 타입과 관계없이 객체를 다뤄야 하거나 객체가 제공하는 메소드나 프로퍼치 이름을 오직 실행시점에 알 수 있는 경우 사용
+* java.lang.reflect와 kotlin.reflect 패키지가 있으며 코틀린 리플렉션의 기능이 충분하지만 복잡한 기능이 필요할때는 자바 리플렉션을 사용해야함
+*
+* */
